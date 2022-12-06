90bc9091e5389af11137d2cab0f58b5b9d2efc2b
Kirti <pahwakirti09942@gmail.com>
2022-12-06 18:54:49 +0530

    login page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,25 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Axios from 'axios';
-import {useState,useEffect} from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 //routes call here
 
-import Auth from './components/organisms/auth/index';
-import Err from './components/organisms/err/index';
-
+import Auth from "./components/organisms/auth/index";
+import Err from "./components/organisms/err/index";
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Auth />} ></Route>
-
-
-        <Route path="/err" element={<Err/>} ></Route>
-        <Route
-            path="*"
-            element={<Navigate to="/err" replace />}
-        />
-
-
-
+        <Route path="/" element={<Auth />}></Route>
+        <Route path="/err" element={<Err />}></Route>
+        <Route path="*" element={<Navigate to="/err" replace />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- client/src/components/organisms/auth/index.jsx
+++ client/src/components/organisms/auth/index.jsx
@@ -1,14 +1,56 @@
-import './index.css';
-import * as React from 'react';
-
+import "./index.css";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Auth = (props) => {
-    return(
-        <>
-        AUTH
-        </>
-    )
-}
-
+  return (
+    <>
+      <div className="outer_container">
+        <aside className="left_box">
+          <div className="credential_container">
+            <div className="heading_container">
+              <h1> Welcome back, Olivia</h1>
+              <p>Welcome back! Please enter your details.</p>
+            </div>
+            <div className="log_with_google_container">
+              <button type="submit">
+                <div className="google_icon">
+                  <FcGoogle size={"23px"} />
+                </div>
+                <p>Log in With Google</p>
+              </button>
+            </div>
+            <p className="horizontal_line">or</p>
+            <main className="inner_container">
+              <form>
+                <input type="email" name="email" placeholder="Email" />
+                <input type="password" name="password" placeholder="Password" />
+                <div className="forgot_pass_container">
+                  <input
+                    type="checkbox"
+                    id="remember"
+                    name="remember"
+                    value="remember"
+                  />
+                  <label className="remember" for="remember">
+                    Remember for 1 week
+                  </label>
+                  <span>
+                    <Link to="/forgotPassword">Forgot Password</Link>
+                  </span>
+                </div>
+                <button className="login_btn" type="submit">
+                  Log in
+                </button>
+              </form>
+            </main>
+          </div>
+        </aside>
+        <aside className="right_box"></aside>
+      </div>
+    </>
+  );
+};
 
 export default Auth;
